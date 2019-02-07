31221bd0669a703dd67268479695f3dc9df7175b
Tymoteusz Paszun <tpaszun@gmail.com>
2019-02-08 00:29:44 +0100

    add workflow trace with max execution constraint

--- src/example.py
+++ src/example.py
@@ -1,17 +1,24 @@
 from Numberjack import *
 
-max_executions = 4
-tasks_count = 5
+max_executions = 2
+tasks_count = 3
+data_entities_count = 4
 
-workflow_trace = VarArray(tasks_count * max_executions)
+max_workflow_trace_count = tasks_count * max_executions
 
-x = Variable(7)
+workflow_trace = VarArray(max_workflow_trace_count, 1, tasks_count, 'task')
 
-model = Model(x > 3)
+process_states = Matrix(max_workflow_trace_count, data_entities_count, -1, 2)
+
+model = Model()
+
+# No more than max_execution occurances of task in workflow trace
+model.add([Cardinality(workflow_trace, x) <= max_executions for x in range(1, tasks_count + 1)])
+
 
 solver = model.load('MiniSat')
 
 solver.startNewSearch()
 
 while solver.getNextSolution() == SAT:
-    print(x)
+    print([x.get_value() for x in workflow_trace])
