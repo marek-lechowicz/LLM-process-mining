b3c96112e292f634ceb2c6ae9ec108525d306a87
Tymoteusz Paszun <tymoteusz.paszun@pl.abb.com>
2019-02-11 09:10:39 +0100

    formating

--- src/main.py
+++ src/main.py
@@ -4,24 +4,24 @@ from utilities import *
 s_0 = [0, 0, 0, 0]
 
 m_tc = [
-    [ 0,  0,  0,  0],
+    [0,  0,  0,  0],
     [-1, -1,  1, -1],
     [-1, -1,  1, -1],
-    [ 1,  1, -1, -1],
+    [1,  1, -1, -1],
     [-1, -1, -1,  0]
 ]
 
 m_te = [
     [-1, -1,  1, -1],
     [-1,  1, -1, -1],
-    [ 1, -1, -1, -1],
+    [1, -1, -1, -1],
     [-1, -1, -1,  1],
     [-1, -1, -1,  1]
 ]
 
 m_st = [
-    [ 1,  1,  1,  0],
-    [ 0,  1,  0,  0],
+    [1,  1,  1,  0],
+    [0,  1,  0,  0],
     [-1, -1, -1,  1]
 ]
 
@@ -29,37 +29,42 @@ assert len(m_tc) == len(m_te)
 assert len(s_0) == len(m_tc[0]) == len(m_te[0]) == len(m_st[0])
 
 
-max_executions = 2 
-tasks_count = 5
-data_entities_count = 4
-max_workflow_trace_count = tasks_count * max_executions # 3. The maximum length of the workflow trace
+max_executions = 2
+tasks_count = len(m_tc)
+data_entities_count = len(s_0)
+# 3. The maximum length of the workflow trace
+max_workflow_trace_count = tasks_count * max_executions
 
 # decision variables
 workflow_trace = VarArray(max_workflow_trace_count, tasks_count, 'task')
-process_states = Matrix(max_workflow_trace_count, data_entities_count, -1, 1, 'state')
+process_states = Matrix(max_workflow_trace_count,
+                        data_entities_count, -1, 1, 'state')
 
 last_task_index = Variable(max_workflow_trace_count)
 
-m_tc_var = Matrix(tasks_count, data_entities_count, -1, 1, 'm_tc')
-m_te_var = Matrix(tasks_count, data_entities_count, -1, 1, 'm_te')
-
-
 model = Model()
 
+# As we want to use m_tc and m_te rows selected by variable we have to
+# create variable matrix (m_tc_var, m_te_var) and bind it's elements
+# to corresponding values from m_tc and m_te.
+m_tc_var = Matrix(tasks_count, data_entities_count, -1, 1, 'm_tc')
+m_te_var = Matrix(tasks_count, data_entities_count, -1, 1, 'm_te')
 model.add(matrixElements(m_tc_var, m_tc))
 model.add(matrixElements(m_te_var, m_te))
 
 # 2. No more than max_execution occurances of task in workflow trace
 model.add([
-    Cardinality(workflow_trace, x) <= max_executions 
+    Cardinality(workflow_trace, x) <= max_executions
     for x in range(0, tasks_count)])
 
 # 4. The input state of the first executed task should be equal to s_0
 model.add([
-    process_states[0, i] == s_0[i] 
+    process_states[0, i] == s_0[i]
     for i in range(0, data_entities_count)])
 
 # 5. Every non-empty task should change the current state.
+
+
 def change_state(i):
     task = workflow_trace[i]
     state = process_states[i]
@@ -67,30 +72,33 @@ def change_state(i):
     effects = m_te_var[task]
     return Conjunction([
         # Equivalent of:
-        # if ((effects[s] == -1) then 
+        # if ((effects[s] == -1) then
         #   next_state[s] == state[s]
-        # else 
-        #   next_state[s] == effects[s]  
-        # 
+        # else
+        #   next_state[s] == effects[s]
+        #
         # Implication as disjunction
-        # p -> q <=> ~p \/ q      
-        
+        # p -> q <=> ~p \/ q
+
         ((effects[s] != -1) | (next_state[s] == state[s]))
         &
-        ((effects[s] ==  -1) | (next_state[s] == effects[s]))
+        ((effects[s] == -1) | (next_state[s] == effects[s]))
 
         for s in range(0, data_entities_count)
     ])
+
 
 model.add([change_state(i) for i in range(0, max_workflow_trace_count - 1)])
 
 # Helpers
 
+
 def state_satisfies_requirements(state, requirements):
-    return Conjunction([ 
-        (state[s] == requirements[s]) | (requirements[s] == -1) 
+    return Conjunction([
+        (state[s] == requirements[s]) | (requirements[s] == -1)
         for s in range(0, len(state))
     ])
+
 
 def state_satisfies_requirements_set(state, requirements_set):
     return Disjunction([
@@ -99,15 +107,18 @@ def state_satisfies_requirements_set(state, requirements_set):
     ])
 
 # 6. The process should end when the desired goal state is achieved.
+
+
 def process_should_end(i):
     state = process_states[i]
     return (
         # Implication as disjunction
         # p -> q <=> ~p \/ q
         (not state_satisfies_requirements(state, m_st[0]))
-        | 
+        |
         (last_task_index < (i + 1))
     )
+
 
 model.add([process_should_end(i) for i in range(0, max_workflow_trace_count)])
 
@@ -118,14 +129,15 @@ last_state = process_states[max_workflow_trace_count - 1]
 
 
 # 8. A task can be executed only if the current state satisfies its input conditions.
-
 def task_condition_check(i):
     task = workflow_trace[i]
     state = process_states[i]
     conditions = m_tc_var[task]
     return state_satisfies_requirements(state, conditions)
 
-model.add([task_condition_check(i) for i in range(0, max_workflow_trace_count)])
+
+model.add([task_condition_check(i)
+           for i in range(0, max_workflow_trace_count)])
 
 # solver = model.load('MiniSat')
 # solver = model.load('WalkSat')
@@ -141,9 +153,9 @@ while solver.getNextSolution() == SAT:
     solution_count += 1
     print(workflow_trace)
     print(process_states)
-    # print(last_task_index)
-    #break
-    solutions.append((VarArray_to_list(workflow_trace), Matrix_to_list(process_states)))
+    print(last_task_index)
+    solutions.append((VarArray_to_list(workflow_trace),
+                      Matrix_to_list(process_states)))
 
 
 print(f'Solutions: {solution_count}')
